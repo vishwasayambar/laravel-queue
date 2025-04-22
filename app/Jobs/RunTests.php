8a2b24b1e60a53761d312835c7d74ffb61818e4a
Vishwa <vishwajitsayambar92@gmail.com>
2025-04-22 09:03:32 +0530

    Learn: Dispatching Workflows - BUS::Chain - BUS::Batch

---
+++ app/Jobs/RunTests.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Batchable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class RunTests implements ShouldQueue
+{
+    use Batchable, Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+//        throw new \Exception('Failed');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Jobs\Deploy;
 use App\Jobs\GenerateReport;
 use App\Jobs\PaymentProcess;
+use App\Jobs\PullRepo;
+use App\Jobs\RunTests;
 use App\Jobs\SendWelcomeEmail;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -51,6 +55,39 @@ Route::get('testJob', function () {
 //        dd('Added In Queue');
 });
 
+//Handling Attempts & Failures
 Route::get('generateReport', function () {
     GenerateReport::dispatch();
+});
+
+//Dispatching Workflows
+Route::get('dispatchingWorkflows', function () {
+    /* CHAIN
+    This all jobs run synchronously as one failed further get stopped execution
+     * */
+    $chain = [
+        new PullRepo(),
+        new RunTests(),
+        new Deploy(),
+    ];
+//    Bus::chain($chain)->dispatch();
+
+    /* BATCH
+    This all jobs run parallel not depend on each other here we can start multiple workers to run
+    them parallel also if one of its get failed it will not stop but it mark as total batch as failed/cancelled even some jobs get run
+    NOTE: ->allowFailures() will avoid to mark cancel whole batch on failed one of its job, as it mark as canceled it again add in queue to run again
+
+    Now to retry the failed job of that batch then run  php artisan queue:retry-batch  9ebbb7bf-d53b-45b2-ae59-f6a37f761ac1 -> BatchId
+    then it will move that failed jobs again in queue for run
+     * */
+
+    $batch = [
+        new PullRepo(),
+        new PullRepo(),
+        new RunTests(),
+        new Deploy(),
+    ];
+    Bus::batch($batch)
+        ->allowFailures()
+        ->dispatch();
 });
