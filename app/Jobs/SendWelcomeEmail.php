7a345f944889d4c9f52ae357625c0a09dc6d99d9
Vishwa <vishwajitsayambar92@gmail.com>
2025-04-22 05:56:59 +0530

    Learn: Job priority to dispatch

--- app/Jobs/SendWelcomeEmail.php
+++ app/Jobs/SendWelcomeEmail.php
@@ -10,7 +10,13 @@ class SendWelcomeEmail implements ShouldQueue
 {
     use Queueable;
 
-    public $timeout = 1; // Now we have sleep(3) in handle means it takes 3sec to complete  but we are failing it in one sec so like this you can give time out to job
+//    public int $timeout = 1; // Now we have sleep(3) in handle means it takes 3sec to complete  but we are failing it in one sec so like this you can give time out to job
+
+    public int $tries = -1; // this is used how many times to retry if it get failed, if you set it -1 then it retry infinite times
+    //You can define one function here called retryUntil() which returns any condition(Ex: time) to till it gets retry
+
+    public int $backoff = 2; // This will add 2 min delay in between try new try like when 1st time fail then it take delay of 2 sec then again retry
+
     /**
      * Create a new job instance.
      */
@@ -25,8 +31,15 @@ class SendWelcomeEmail implements ShouldQueue
      */
     #[NoReturn] public function handle(): void
     {
-        sleep(3);
-//        throw new \Exception();
+        throw new \Exception('Failed');
+//        sleep(3);
 //        dd('Sending welcome email');
+    }
+
+
+//    Till this condition get true it will retries
+    public function retryUntil(): \Illuminate\Support\Carbon
+    {
+        return now()->addMinutes(1);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Jobs\PaymentProcess;
+use App\Jobs\SendWelcomeEmail;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -28,8 +30,21 @@ require __DIR__.'/auth.php';
 
 
 Route::get('testJob', function () {
-//    (new \App\Jobs\SendWelcomeEmail())->handle(); //Without Queue
-//        \App\Jobs\SendWelcomeEmail::dispatch()->delay(now()->addMinutes(5)); // This will Delay the processing of job by worker for 5 min even worker is there it it will not process till 5 min
-        \App\Jobs\SendWelcomeEmail::dispatch();
-        dd('Added In Queue');
+//    (new SendWelcomeEmail())->handle(); //Without Queue
+//        SendWelcomeEmail::dispatch()->delay(now()->addMinutes(5)); // This will Delay the processing of job by worker for 5 min even worker is there it will not process till 5 min
+//    SendWelcomeEmail::dispatch();
+
+
+//    Now suppose we have lots of job to process like 100 and each job taking long time to process but you have to some imp Job which you want to run on priority before that current running jobs
+    // like queue work as FIFO then it will not allow to run which come last to run first like below Example
+    foreach (range(1, 100) as $i) {
+        SendWelcomeEmail::dispatch();
+    }
+//    Now above it taking more time to run but below is Payment job it should run first then how then there is queue priority comes
+//Solution One = Run multiple workers like two separate tabs run php artisan queue:work so it will complete fast but
+// Solution 2 = High priority task run on separate queue and while running worker there is method option to set queue priority
+//like php artisan queue:work --queue=payment,default like this now jobs in payment queue will run fast even default have job pending to run
+//bydefault it runs on default queue if we not provide such options
+        PaymentProcess::dispatch()->onQueue('payment');
+//        dd('Added In Queue');
 });
